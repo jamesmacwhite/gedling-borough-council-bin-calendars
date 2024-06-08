eb145ec36e9e401ec969d6d12899c081390b2bb6
James White <james@jmwhite.co.uk>
2024-06-08 19:11:44 +0100

    Adjust log message

--- ical2json.js
+++ ical2json.js
@@ -1,14 +1,17 @@
 'use strict';
+
 const path = require('path');
 const ical2json = require("ical2json");
 const jetpack = require("fs-jetpack");
 const calendars = jetpack.find("ical", { matching: "*.ics" });
 const dest = './json';
 
+// Remove json folder if exists
 jetpack.cwd(dest).remove();
 
+// Process each ical file and use ical2json to convert original data and output as JSON file in json folder
 calendars.forEach(function(filepath) {
-    console.log(`Converting ${filepath} to JSON.`)
     var convertedJSON = ical2json.convert(jetpack.read(filepath));
-    jetpack.write(`./json/${path.parse(filepath).name}.json`, convertedJSON);
+    console.log(`Converted ${filepath} to JSON.`);
+    jetpack.write(`${dest}/${path.parse(filepath).name}.json`, convertedJSON);
 });
