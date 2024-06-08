dce086bd477bc07084f29d8a95853ed062e9489e
James White <james@jmwhite.co.uk>
2024-06-08 19:08:38 +0100

    Remove json folder before writing

--- ical2json.js
+++ ical2json.js
@@ -3,6 +3,9 @@ const path = require('path');
 const ical2json = require("ical2json");
 const jetpack = require("fs-jetpack");
 const calendars = jetpack.find("ical", { matching: "*.ics" });
+const dest = './json';
+
+jetpack.cwd(dest).remove();
 
 calendars.forEach(function(filepath) {
     console.log(`Converting ${filepath} to JSON.`)
