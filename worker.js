fba2a8b6a55e9e35d4b0dcba26ef38d553e9a19a
James White <james@jmwhite.co.uk>
2024-06-20 00:35:33 +0100

    Refactor worker to support multiple collection dates

--- worker.js
+++ worker.js
@@ -1,13 +1,30 @@
+import { parse } from 'node-html-parser';
+
 export default {
   async fetch(request) {
     let viewState = '';
     let viewStateGenerator = '';
     let eventValidation = '';
-    let refuseCollectionEmailSubscribeUrl = '';
-    let gardenWasteCollectionEmailSubscribeUrl = '';
+
+    const refuseCollectionJsonKeys = ['Location', 'Area', 'Calendar URL', 'Email Subscribe URL', 'Schedule Identifier', 'Schedule Name'];
+    const gardenWasteCollectionJsonKeys = ['Location', 'Numbers', 'Area', 'Calendar URL', 'Email Subscribe URL', 'Schedule Identifier', 'Schedule Name'];
+    const gedlingAppDomainUrl = 'https://apps.gedling.gov.uk';
+    
+    let refuseCollectionData = [];
+    let gardenWasteCollectionData = [];
+    let refuseRowsHtml = '';
+    let gardenWasteRowsHtml = '';
 
     function getCollectionIdentifier(url) {
       return url.split("/").pop() || null;
+    }
+
+    function formatRefuseCalendarPDFUrl(path) {
+      return `${gedlingAppDomainUrl}/refuse/${path}`;
+    }
+
+    function formatGardenCalendarPDFUrl(path) {
+      return `${gedlingAppDomainUrl}/${path.replace('../', '')}`;
     }
 
     function formatCollectionName(url) {
@@ -35,6 +52,13 @@ export default {
 
     const url = new URL(request.url);
     const streetName = url.searchParams.get('streetName');
+
+    if (request.method !== 'GET') {
+      return new Response('This worker only supports GET requests', {
+        status: 400,
+        headers: corsHeaders
+      })
+    }
 
     if (!streetName) {
       return new Response('Missing street name parameter', { 
@@ -73,34 +97,122 @@ export default {
       .transform(searchPage)
       .text();
 
-      // Build POST parameters
-      let formData = new FormData();
-      formData.append('__VIEWSTATE', viewState);
-      formData.append('__EVENTVALIDATION', eventValidation);
-      formData.append('ctl00$MainContent$street', streetName);
-      formData.append('ctl00$MainContent$mybutton', 'Search');
+    // Build POST parameters
+    let formData = new FormData();
+    formData.append('__VIEWSTATE', viewState);
+    formData.append('__EVENTVALIDATION', eventValidation);
+    formData.append('ctl00$MainContent$street', streetName);
+    formData.append('ctl00$MainContent$mybutton', 'Search');
 
-      const searchRequest = await fetch(searchUrl, {
-        method: 'POST',
-        body: formData
+    const searchRequest = await fetch(searchUrl, {
+      method: 'POST',
+      body: formData
+    });
+
+    await new HTMLRewriter()
+      .on('table#ctl00_MainContent_streetgridview tbody', {
+        text({ text }) { 
+          refuseRowsHtml += text 
+        },
+      })
+      .on('table#ctl00_MainContent_streetgridview tbody *', {
+        element(el) { 
+          const attrs = [...el.attributes].map(([k, v]) => ` ${k}="${v}"`).join('');
+          refuseRowsHtml += `<${el.tagName}${attrs}>`;
+          el.onEndTag(endTag => { 
+            refuseRowsHtml += `</${endTag.name}>`;
+          });
+        }
+      })
+      .on('table#ctl00_MainContent_gardenGridView tbody', {
+        text({ text }) { gardenWasteRowsHtml += text }
+      })
+      .on('table#ctl00_MainContent_gardenGridView tbody *', {
+        element(el) {
+          const attrs = [...el.attributes].map(([k, v]) => ` ${k}="${v}"`).join('');
+          gardenWasteRowsHtml += `<${el.tagName}${attrs}>`;
+          el.onEndTag(endTag => { 
+            gardenWasteRowsHtml += `</${endTag.name}>`;
+          });
+        }
+      })
+      .transform(searchRequest)
+      .text()
+
+    let refuseData = parse(refuseRowsHtml).querySelectorAll('tr');
+    let gardenWasteData = parse(gardenWasteRowsHtml).querySelectorAll('tr');
+
+    let subscribeUrl = '';
+
+    refuseData.forEach((row) => {
+      const cells = row.removeWhitespace().querySelectorAll('td');
+      const rowData = cells.map(function(cell) {
+
+      if (cell.text === 'Download Calendar' || cell.text === 'Subscribe') {
+        let href = cell.querySelector('a').getAttribute('href');
+
+        if (cell.text === 'Download Calendar') {
+            return formatRefuseCalendarPDFUrl(href);
+        }
+
+        if (cell.text === 'Subscribe') {
+          subscribeUrl = href;
+          return href;
+        }
+      }
+
+        return cell.text;
+
       });
 
-      await new HTMLRewriter()
-        .on('a#ctl00_MainContent_streetgridview_ctl02_Hyperlink1', {
-          element(elem) {
-            refuseCollectionEmailSubscribeUrl = elem.getAttribute('href');
-          }
-        })
-        .on('a#ctl00_MainContent_gardenGridView_ctl02_Hyperlink2', {
-          element(elem) {
-            gardenWasteCollectionEmailSubscribeUrl = elem.getAttribute('href');
-          }
-        })
-        .transform(searchRequest)
-        .text()
+      rowData.push(getCollectionIdentifier(subscribeUrl));
+      rowData.push(formatCollectionName(subscribeUrl));
 
+      const rowObject = {};
 
-    if (!refuseCollectionEmailSubscribeUrl || !gardenWasteCollectionEmailSubscribeUrl) {
+      for (let i = 0; i < refuseCollectionJsonKeys.length; i++) {
+          rowObject[refuseCollectionJsonKeys[i]] = rowData[i];
+      }
+
+      refuseCollectionData.push(rowObject);
+
+    });
+
+    gardenWasteData.forEach((row) => {
+      const cells = row.removeWhitespace().querySelectorAll('td');
+      const rowData = cells.map(function(cell, index) {
+
+      if (cell.text === 'Download Calendar' || cell.text === 'Subscribe') {
+        let href = cell.querySelector('a').getAttribute('href');
+
+        if (cell.text === 'Download Calendar') {
+            return formatGardenCalendarPDFUrl(href);
+        }
+
+        if (cell.text === 'Subscribe') {
+          subscribeUrl = href;
+          return href;
+        }
+      }
+
+        return cell.text || null;
+
+      });
+
+      rowData.push(getCollectionIdentifier(subscribeUrl));
+      rowData.push(formatCollectionName(subscribeUrl));
+
+      const rowObject = {};
+
+      for (let i = 0; i < gardenWasteCollectionJsonKeys.length; i++) {
+          rowObject[gardenWasteCollectionJsonKeys[i]] = rowData[i];
+      }
+
+      gardenWasteCollectionData.push(rowObject);
+
+    });
+
+    if (refuseCollectionData.length === 0 && gardenWasteCollectionData.length === 0) {
       return new Response('The street name entered did not return any bin collection data. Please check the street name entered is valid, spelt correctly and within the Gedling district and try again.', {
         status: 404,
         headers: corsHeaders
@@ -109,10 +221,8 @@ export default {
 
     return new Response(JSON.stringify({
       'streetNameQuery': streetName,
-      'refuseCollectionScheduleId': getCollectionIdentifier(refuseCollectionEmailSubscribeUrl),
-      'refuseCollectionScheduleName': formatCollectionName(refuseCollectionEmailSubscribeUrl),
-      'gardenWasteCollectionScheduleId': getCollectionIdentifier(gardenWasteCollectionEmailSubscribeUrl),
-      'gardenWasteCollectionScheduleName': formatCollectionName(gardenWasteCollectionEmailSubscribeUrl),
+      'refuseCollections': refuseCollectionData,
+      'gardenWasteCollections': gardenWasteCollectionData,
       'viewState': viewState || null,
       'viewStateGenerator': viewStateGenerator || null,
       'eventValidation': eventValidation || null
